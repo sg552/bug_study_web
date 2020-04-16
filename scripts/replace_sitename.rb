0530864663049de09cde8ea8f125acd21b871358
申思维 <shensiwei@sina.com>
2020-04-17 07:38:44 +0800

    修改了script

--- scripts/replace_sitename.rb
+++ scripts/replace_sitename.rb
@@ -3,12 +3,10 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'rails'
 require 'rubygems'
 
-bug = Bug.find(88818).wybug_detail
-
 Bug.all.each do |bug|
   puts "== doing: #{bug.id}"
   old_content = bug.wybug_detail
-  new_content = old_content.gsub("http://static.loner.fm/upload", "http://showmethemoney.hnengdata.com/image_security/10-16")
+  new_content = old_content.gsub("http://static.loner.fm/upload", "YOUR_CDN_SERVER_AND_PATH")
   bug.wybug_detail = new_content
   bug.save!
 end
